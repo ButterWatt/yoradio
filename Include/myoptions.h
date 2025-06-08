e1dd770ac1af01576eed77f09b918cf2ffdbe8ac
ButterWatt <ButterWatt@protonmail.com>
2025-06-08 21:27:16 +0700

    Create myoptions.h for ESP32-2432S028

---
+++ Include/myoptions.h
@@ -0,0 +1,23 @@
+#ifndef myoptions_h
+#define myoptions_h
+
+#define DSP_MODEL DSP_ILI9341
+#define DSP_HSPI true
+#define LED_INVERT true
+#define TFT_CS 15
+#define TFT_DC 2
+#define TFT_RST -1
+#define LED_BUILTIN 17
+#define BRIGHTNESS_PIN 21
+#define I2S_INTERNAL true
+#define PLAYER_FORCE_MONO true
+//#define I2S_DOUT 22
+//#define I2S_BCLK 16
+//#define I2S_LRC 4
+#define TS_MODEL TS_MODEL_XPT2046
+#define TS_SPIPINS 25, 39, 32 /* SCK, MISO, MOSI */
+#define TS_CS 33
+//#define IR_PIN 27
+#define LIGHT_SENSOR 34
+#define AUTOBACKLIGHT_MAX 1024
+#endif
